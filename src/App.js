778970d937a7160830a8727e3ba541d1be187e9b
unknown <pdullabh315@gmail.com>
2020-12-22 16:52:06 +0200

    deploying frontend

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import 'tachyons';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register'
-import { render } from '@testing-library/react';
+//import { render } from '@testing-library/react';
 
 
 const particleOptions = {
@@ -19,23 +18,47 @@ const particleOptions = {
 }
 }
 
-const app = new Clarifai.App({
-  apiKey: '1671dc52247642daa536e4c5ec8a28a2'
- });
+
+
+
+const inialState = 
+  {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'SignIn',
+    isSignedIn: false,
+    user: {
+      id: '',
+      name: '',
+      email: '',
+      password: '',
+      entries: 0,
+      joined : 'new Date()'
+    }
+  }
 
 
 class App extends Component {
-  
+
   constructor(){
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'SignIn',
-      isSignedIn: false
-    }
+    this.state = inialState;
   }
+
+  loadUser = (data) => {
+    this.setState(
+      {user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        password: data.password,
+        entries: data.entries,
+        joined : data.joined
+       } }
+    )
+  }
+
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
@@ -61,16 +84,28 @@ class App extends Component {
   
   onButtonSubmit = () => {
       this.setState({imageUrl : this.state.input})
-      app.models
-      .initModel({
-        id: Clarifai.FACE_DETECT_MODEL,
+      fetch('https://warm-caverns-83731.herokuapp.com/imageurl', {
+        method : 'post',
+        headers : {'Content-Type' : 'application/json'},
+        body : JSON.stringify({
+        input : this.state.input
+        })
       })
-      .then((model) => {
-        return model.predict(
-          this.state.input
-        );
-      })
+      .then(response => response.json())
       .then((response) => {
+        if (response){
+          fetch('https://warm-caverns-83731.herokuapp.com/image', {
+            method : 'put',
+            headers : {'Content-Type' : 'application/json'},
+            body : JSON.stringify({
+                id : this.state.user.id
+            })
+        })
+            .then(response => response.json())
+            .then(count => {
+                this.setState(Object.assign(this.state.user, {entries : count}))
+            })
+        }
         this.displayFaceBox(this.calculateFaceLocation(response));
       })
       .catch(err => this.setState({box: {}})); //my own improvement
@@ -79,7 +114,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'SignIn' || route === 'Register'){
-      this.setState({isSignedIn : false})
+      this.setState(inialState)
     } else if (route === 'Home'){
       this.setState({isSignedIn : true})
     }
@@ -97,14 +132,14 @@ class App extends Component {
           { this.state.route ==='Home' 
             ? <div>
             <Logo />
-            <Rank />
+            <Rank name = {this.state.user.name} entries = {this.state.user.entries} />
             <ImageLinkForm onButtonSubmit = {this.onButtonSubmit} onInputChange = {this.onInputChange}/>
             <FaceRecognition box = {this.state.box} imagePrediction = {this.state.imageUrl}/>
           </div>
             : (
               this.state.route ==='SignIn'
-              ? <SignIn onRouteChange = {this.onRouteChange} />
-              : <Register onRouteChange = {this.onRouteChange} />
+              ? <SignIn loadUser = {this.loadUser} onRouteChange = {this.onRouteChange} />
+              : <Register loadUser = {this.loadUser} onRouteChange = {this.onRouteChange} />
             )
           }     
       </div>
